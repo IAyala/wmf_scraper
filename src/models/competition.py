b854646dfb04564c675917353bd61f36a5af4bb2
Ivan Ayala <ivan.ayala.alcalde@gmail.com>
2023-09-26 09:23:09 +0000

    Add database documentation, amend table models. WIP

--- src/models/competition.py
+++ src/models/competition.py
@@ -4,19 +4,10 @@ from typing import List, Optional
 from sqlmodel import Field, Relationship, SQLModel
 
 
-class CompetitionRequest(SQLModel):
-    url: str
-    description: str
-
-
-class LoadCompetitionRequest(SQLModel):
-    competition_id: int
-
-
-class CompetitionModel(CompetitionRequest, table=True):  # type: ignore
+class CompetitionModel(SQLModel, table=True):  # type: ignore
     competition_id: Optional[int] = Field(default=None, primary_key=True, index=True)
-    url: str = Field(unique=True)
-    description: str = Field(unique=True)
-    load_time: Optional[datetime] = Field(default=None)
+    competition_url: str = Field(unique=True)
+    competition_description: str = Field(unique=True)
+    competition_load_time: Optional[datetime] = Field(default=None)
 
     tasks: List["TaskModel"] = Relationship(back_populates="competition")  # type: ignore # noqa
--- src/models/competitor.py
+++ src/models/competitor.py
@@ -3,12 +3,7 @@ from typing import Optional
 from sqlmodel import Field, SQLModel
 
 
-class CompetitorSimpleModel(SQLModel):
-    name: str = Field(unique=True, nullable=False)
-    country: str = Field(nullable=False)
-
-
-class CompetitorModel(CompetitorSimpleModel, table=True):  # type: ignore
-    competitor_id: Optional[int] = Field(
-        default=None, primary_key=True, index=True, nullable=False
-    )
+class CompetitorModel(SQLModel, table=True):  # type: ignore
+    competitor_id: Optional[int] = Field(default=None, primary_key=True, index=True)
+    competitor_name: str = Field(unique=True, nullable=False)
+    competitor_country: str = Field(nullable=False)
