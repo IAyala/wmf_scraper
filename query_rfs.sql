c09441abfc4e9b2306dc484ba930b59e2671dd41
Ivan Ayala <ivan.ayala.alcalde@gmail.com>
2024-09-17 11:19:49 +0000

    Useful query added

---
+++ query_rfs.sql
@@ -0,0 +1,14 @@
+select
+    com.competitor_name,
+    tm.task_order,
+    tm.task_name,
+    trm.tr_task_penalty,
+    trm.tr_competition_penalty,
+    trm.tr_notes
+from taskmodel tm
+    join taskresultmodel trm on tm.task_id = trm.task_id
+    join competitionmodel cm on tm.competition_id = cm.competition_id
+    join competitormodel com on com.competitor_id = trm.competitor_id
+where
+    cm.competition_id = 12
+    and trm.tr_notes like '%10.2%' order by com.competitor_name, tm.task_order;
